4a43bc93268d3294cb8712ce775262993db40bd6
Lucas Esteves A. Coelho <lucas.coelho@portoseguro.com.br>
2023-04-25 14:53:31 -0300

    Updating Project Structure

---
+++ js/traveller.js
@@ -0,0 +1,12 @@
+$(document).ready(function ()
+{
+    function click()
+    {
+        $('#dialog').dialog({
+            autoOpen: false,
+            width: 250,
+            height: 180,
+            modal : true
+        });
+    }
+});
