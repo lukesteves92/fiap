e9fdf2647d8ea9ee975c9a26b9a1bb551f251174
Lucas Esteves Albuquerque Coelho <lucas.esteves@Lucass-MacBook-Pro.local>
2024-09-20 19:58:22 -0300

    Updating README

--- gulliver/src/main/java/br/com/fiap/gt/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.gt.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-}
